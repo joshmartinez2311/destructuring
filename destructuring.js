20e0377769ed45bc46858b8419aa61f09c861458
Joshthe1st <88605608+Joshthe1st@users.noreply.github.com>
2023-05-02 17:30:22 -0700

    first commit

---
+++ destructuring.js
@@ -0,0 +1,70 @@
+
+//problem 1
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+ //first one will print Tesla
+ // second will print Mercedes
+
+//problem 2
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+//console.log(name);
+//console.log(otherName);
+
+//name will error out
+//other name will print Elon
+
+//Problem 3
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+// password will print since its outside of the person object
+// hasedPasword will be undefine
+
+
+// Problem 4
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+// none will error out
+// correction first is false since first=[2] and second=[5]
+// second console.log is true since first=[2] and third=[2]
+
+//problem 5
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+// will print all the values
+//will print the secondKey array 
+// will error out, correction will print [1]
+//will print 5
